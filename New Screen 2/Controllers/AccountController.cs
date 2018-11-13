c4233230d9e29ee33fab59e53172396c0a5f801f
KhoaPham <phamdangkhoa0403@gmail.com>
2018-11-13 08:22:57 +0700

    first commit

---
+++ New Screen 2/Controllers/AccountController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using New_Screen_2.Models;
+namespace New_Screen_2.Controllers
+{
+    public class AccountController : Controller
+    {
+        //
+        // GET: /Account/
+        DemoTeam12Entities db = new DemoTeam12Entities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(string username, string password)
+        {
+            var user = db.Users.FirstOrDefault(x => x.Username == username);
+            if (user != null)
+            {
+                if (user.Password.Equals(password))
+                {
+                    Session["Username"] = user.Username;
+                    Session["UserID"] = user.id;
+                   
+                    {
+                        return RedirectToAction("Index", "Home");
+                    }
+                }
+           
+              
+            }
+            else
+            {
+                ViewBag.mgs = "User chưa đăng kí, vui lòng đăng kí !";
+            }
+           
+            return View();
+        }
+
+    }
+}
---
+++ New Screen 2/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using New_Screen_2.Models;
+namespace New_Screen_2.Controllers
+{
+    public class HomeController : Controller
+    {
+        DemoTeam12Entities db = new DemoTeam12Entities();
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your app description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        
+               public ActionResult Newtask()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
